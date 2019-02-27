50ba9decface4c550ba8d883a1f8e1c937a0d849
Adam Saxton <ams17a@acu.edu>
2019-02-27 07:48:02 -0600

    Initial Commit

---
+++ PamphletUIContraintProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PamphletUIContraintProject
+//
+//  Created by Adam Saxton on 2/27/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
